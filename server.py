ae893bd525867eb8b962fc74d4a7a9884cc1eb28
FaztSquirrel <redisconnect@gmail.com>
2018-05-12 12:57:46 +0200

    Python aangepast zodat de nieuwe csv's gebruikt kunnen worden.

--- server.py
+++ server.py
@@ -5,26 +5,35 @@ import tornado.web
 
 import csv
 
+def select_all(file):
+  rijen = []
+
+  with open(file, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+
+    for rij in csvreader: rijen.append(rij)
+
+  return rijen
+
+
 class MainHandler(tornado.web.RequestHandler):
-    def get(self, i_dont_care):
-    	with open('index.html', 'r') as template:
-	    	with open('Essences.csv', 'r') as csvfile:
-	    		csvreader = csv.reader(csvfile)
+  def get(self, i_dont_care):
+    with open('index.html', 'r') as template:
+      essences = select_all('Essences.csv')
+      tips = select_all('tips.csv')
+      todos = select_all('todos.csv')
 
-	    		rijen = []
-	    		for rij in csvreader: rijen.append(rij)
+      t = tornado.template.Template(template.read())
 
-	    		t = tornado.template.Template(template.read())
-
-	    		self.write(t.generate(csv=rijen))
+      self.write(t.generate(essences=essences, tips=tips, todos=todos))
 
 def make_app():
-    return tornado.web.Application([
-    	(r"/(|index.php)", MainHandler),
-        (r"/(.*)", tornado.web.StaticFileHandler, {'path': "."}),
-    ])
+  return tornado.web.Application([
+    (r"/(|index.php)", MainHandler),
+      (r"/(.*)", tornado.web.StaticFileHandler, {'path': "."}),
+  ])
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(5000)
-    tornado.ioloop.IOLoop.current().start()
+  app = make_app()
+  app.listen(5000)
+  tornado.ioloop.IOLoop.current().start()
